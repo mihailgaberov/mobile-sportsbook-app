7fa92a4732f9a71b1e9158ce91686446a30f7d4d
Mihail Gaberov <mihail.gaberov@williamhill.com>
2019-06-19 09:46:35 +0200

    Updated gulpfile due to staled versions in order to try a fresh run.

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,12 +1,14 @@
 // Load gulp packages
 var gulp = require('gulp'),
-	source = require('vinyl-source-stream'),
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
 	cssnano = require('gulp-cssnano'),
 	notify = require('gulp-notify'),
 	rename = require('gulp-rename'),
 	sass = require('gulp-sass');
+
+const { src, dest, parallel } = require('gulp');
+
 
 var input = {
 		'styles': 'src/styles/**/*.scss',
@@ -18,45 +20,40 @@ var input = {
 	};
 
 // Default task
-gulp.task('default', function () {
-	gulp.start('styles', 'scripts', 'foundation-offcanvas');
+gulp.task('default', gulp.series(), function() {
+	gulp.watch('styles', 'scripts', 'foundation-offcanvas');
 });
 
-// Deploy the styles
-gulp.task('styles', function () {
-	return gulp.src(input.styles, {style: 'expanded'})
+
+function css() {
+	return src(input.styles, {style: 'expanded'})
 		.pipe(sass())
 		.pipe(concat('main.css'))
-		.pipe(gulp.dest(output.styles))
+		.pipe(dest(output.styles))
 		.pipe(rename({suffix: '.min'}))
 		.pipe(cssnano())
-		.pipe(gulp.dest(output.styles))
+		.pipe(dest(output.styles))
 		.pipe(notify({message: 'Styles task complete'}));
-});
+}
 
 // Deploy the scripts
-gulp.task('scripts', function () {
-	return gulp.src(input.scripts)
+function js() {
+	return src(input.scripts)
 		.pipe(concat('main.js'))
-		.pipe(gulp.dest(output.scripts))
+		.pipe(dest(output.scripts))
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
-		.pipe(gulp.dest(output.scripts))
+		.pipe(dest(output.scripts))
 		.pipe(notify({message: 'Scripts task complete'}));
-});
+}
 
-// Foundation components - offcanvas
-gulp.task('foundation-offcanvas', function () {
-return gulp.src(['./node_modules/foundation-apps/js/angular/components/offcanvas/*'])
-    .pipe(gulp.dest('./components/offcanvas/'))
-    .pipe(notify({message: 'Foundation Offcanvas task complete'}));
-});
+function foundationCanvas() {
+	return src(['./node_modules/foundation-apps/js/angular/components/offcanvas/*'])
+		.pipe(dest('./components/offcanvas/'))
+		.pipe(notify({message: 'Foundation Offcanvas task complete'}));
+}
 
-// Watch
-gulp.task('watch', function () {
-	// Watch .scss files
-	gulp.watch('src/styles/**/*.scss', ['styles']);
-
-	// Watch .js files
-	gulp.watch('src/scripts/**/*.js', ['scripts']);
-});
+exports.js = js;
+exports.css = css;
+exports.foundationCanvas = foundationCanvas();
+exports.default = parallel(css, js, foundationCanvas);
